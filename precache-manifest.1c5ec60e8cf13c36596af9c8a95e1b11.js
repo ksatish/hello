f3f16a454af1ccbdb9e4379e1e3db840c0e92f0d
satish.kadam <satish.kadam@indecomm.com>
2020-07-31 17:39:00 +0530

    Updates

---
+++ precache-manifest.1c5ec60e8cf13c36596af9c8a95e1b11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "975ed435826cd9c6b739a5e4cf17f7af",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "c652a5945e71a5b99164",
+    "url": "/hello/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4166fe4a8ffbe406d4c0",
+    "url": "/hello/static/js/2.5bd2fb6a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello/static/js/2.5bd2fb6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c652a5945e71a5b99164",
+    "url": "/hello/static/js/main.846308a2.chunk.js"
+  },
+  {
+    "revision": "a8febeeed84765992edc",
+    "url": "/hello/static/js/runtime-main.1381f6d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hello/static/media/logo.5d5d9eef.svg"
+  }
+]);
